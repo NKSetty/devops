62ec9b83dc954b5eb1ab3fc56fbdb6ac317d9b3c
Nik <settynikhilesh@gmail.com>
2021-06-25 17:35:28 +0000

    new line called -am was tested in backup file

--- backup.sh
+++ backup.sh
@@ -3,3 +3,4 @@ I edited this file on github online site
 Added third line to the code
 this is my fourth line
 This is my fithline to test diff command
+Trying new code called Git Commit -am in the code
